a45b6f4f950f23c6d4a180a4b98e2ed91ed0c3bb
Algorithm <algori8m@gmail.com>
2021-09-03 22:52:33 +0100

    minor changes

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,11 @@ function showMenu(){
   if(navList.classList.contains("show")){
     navList.classList.remove("show")
     header.style.boxShadow = "0px 1px 3px 1px rgba(87,87,87,0.525)";
+    harmburger.classList.remove("active");
   }else{
     header.style.boxShadow = "none";
     navList.classList.add("show");
+    harmburger.classList.add("active");
   }
 }
 
@@ -20,3 +22,5 @@ harmburger.addEventListener("click",showMenu);
 navLinks.forEach((link)=>{
   link.addEventListener("click",showMenu)
 })
+
+console.log("Hello");
